98cddf52b0770ab75b7f17df14491d9c2f3b56d8
YISHEN CAI <yisen614@gmail.com>
2023-07-08 14:23:57 +0800

    add rolling file config

--- src/log4rs_example.rs
+++ src/log4rs_example.rs
@@ -1,13 +1,18 @@
-use log::{debug, info, warn, error};
+mod functions;
+mod add_ons;
 
+use log::{debug, info, warn, error};
+use add_ons::{};
 
 #[allow(dead_code)]
 fn main() {
     log4rs::init_file("log4rs.yml", Default::default()).unwrap();
 
-    debug!("Mary has a little lamb");
-    debug!(target: "debug.log", "Specify debug log!!!");
-    error!("{}", "Its fleece was white as snow");
-    info!("{:?}", "And every where that Mary went");
-    warn!("{:#?}", "The lamb was sure to go");
+    while true {
+        debug!("Mary has a little lamb");
+        debug!(target: "debug.log", "Specify debug log!!!");
+        error!("{}", "Its fleece was white as snow");
+        info!("{:?}", "And every where that Mary went");
+        warn!("{:#?}", "The lamb was sure to go");
+    }
 }
