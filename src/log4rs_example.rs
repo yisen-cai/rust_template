75517c85daf20df0d858bbfda30083438df0cddb
YISHEN CAI <yisen614@gmail.com>
2023-07-08 13:58:29 +0800

    clean logger code config

--- src/log4rs_example.rs
+++ src/log4rs_example.rs
@@ -1,33 +1,8 @@
-use log::{debug, info, warn, error, LevelFilter};
-use log4rs::append::console::ConsoleAppender;
-use log4rs::append::file::FileAppender;
-use log4rs::config::{Appender, Root};
-use log4rs::Config;
-// use log4rs::encode::pattern::PatternEncoder;
+use log::{debug, info, warn, error};
 
 
 #[allow(dead_code)]
 fn main() {
-    // let stdout = ConsoleAppender::builder().build();
-    // let info_log = FileAppender::builder()
-    //     // .encoder(Box::new(PatternEncoder::new("{l} - {m}\n")))
-    //     .build("logs/info.log").unwrap();
-    // let debug_log = FileAppender::builder()
-    //     .build("logs/debug.log").unwrap();
-    //
-    // let config = Config::builder()
-    //     .appender(Appender::builder().build("stdout", Box::new(stdout)))
-    //     .appender(Appender::builder().build("info_log", Box::new(info_log)))
-    //     .appender(Appender::builder().build("debug_log", Box::new(debug_log)))
-    //     .build(
-    //         Root::builder()
-    //             .appender("stdout")
-    //             .appender("info_log")
-    //             .appender("debug_log")
-    //             .build(LevelFilter::Trace))
-    //     .unwrap();
-    //
-    //
     log4rs::init_file("log4rs.yml", Default::default()).unwrap();
 
     debug!("Mary has a little lamb");
